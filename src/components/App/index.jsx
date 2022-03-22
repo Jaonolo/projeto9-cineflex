c3e0dbccb71a1cb496e166f19f956a759be01d4a
João Vitor Sanches <joaovitormanolo@gmail.com>
2022-03-22 15:51:23 -0300

    feat : initial file structure and creating header component

---
+++ src/components/App/index.jsx
@@ -0,0 +1,9 @@
+
+import Header from '../Header'
+import './style.css'
+
+const App = () => <>
+    <Header />
+</>
+
+export default App
---
+++ src/components/Header/index.jsx
@@ -0,0 +1,7 @@
+import './style.css'
+
+const Header = () => <header class="header">
+    CINEFLEX
+</header>
+
+export default Header
