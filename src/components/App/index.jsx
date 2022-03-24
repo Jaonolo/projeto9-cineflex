891bf6130e84cfcb879e69f28739d813d187457d
João Vitor Sanches <joaovitormanolo@gmail.com>
2022-03-23 22:57:38 -0300

    fix and feat : adding useEffect to axios calls and start implementing router functionality

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -1,10 +1,14 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+
 import Header from '../Header'
 import Home from '../Home'
 import './style.css'
 
-const App = () => <>
+const App = () => <BrowserRouter>
     <Header />
-    <Home />
-</>
+    <Routes>
+        <Route path="/" element={<Home />}></Route>
+    </Routes>
+</BrowserRouter>
 
 export default App
--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -1,20 +1,27 @@
 import axios from 'axios'
-import { useState } from 'react'
-
+import { useState, useEffect } from 'react'
+import { Link } from 'react-router-dom'
+ 
 import './style.css'
 
 const Home = () => {
     const [movies, setMovies] = useState([])
 
-    axios.get('https://mock-api.driven.com.br/api/v5/cineflex/movies').then(response => setMovies(response.data))
-
+    useEffect(
+        () => axios
+                .get('https://mock-api.driven.com.br/api/v5/cineflex/movies')
+                .then(({data}) => setMovies(data))
+    , [])
+    
     return <main className="home">
         <h1>Selecione o filme</h1>
         <div>
-            {movies.map((e, i) =>
-                <div className="frame"> 
-                    <img src={e.posterURL} key={`movie-${i}`}/>
-                </div>
+            {movies.map(e =>
+                <Link to={`/filme/${e.id}`} key={`movie-${e.id}`}>
+                    <div className="frame"> 
+                        <img src={e.posterURL} alt={`movie-${e.id}`}/>
+                    </div>
+                </Link>
             )}
         </div>
     </main>
